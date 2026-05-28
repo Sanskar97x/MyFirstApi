676d21b921e3f54676d1cb1ac6125d237f694b98
Sanskar97x <tamangsanskar3@gmail.com>
2026-05-28 13:40:52 +0545

    Initial Commit

---
+++ Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstApi.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string ? Name { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+
+        public DateOnly?DOB { get; set; }
+
+        public string? Postition { get; set; }
+
+        public string?Department { get; set; }
+
+        public string? EmailAddress { get; set; }
+    }
+}
