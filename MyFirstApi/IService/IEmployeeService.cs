9390a8498d2f8fcfc782c58f3919b080f7ee19b3
Sanskar97x <tamangsanskar3@gmail.com>
2026-06-05 12:47:11 +0545

    Commit Nr 7

--- MyFirstApi/IService/IEmployeeService.cs
+++ MyFirstApi/IService/IEmployeeService.cs
@@ -5,5 +5,6 @@ namespace MyFirstApi.IService
     public interface IEmployeeService
     {
         Task<Tuple<int, List<EmployeeDto>>> GetAllEmployeeAsync();
+        Task<Tuple<int, string>> CreateEmployee(EmployeeDto employee);
     }
 }
