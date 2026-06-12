ed5f9afcea902cadde9ef242b5faf17adf8d423e
Sanskar97x <tamangsanskar3@gmail.com>
2026-06-12 12:46:32 +0545

    Add default empty string values for email and password

--- MyFirstApi/Dto/UserDto.cs
+++ MyFirstApi/Dto/UserDto.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string? Name { get; set; }
-        public required string Email { get; set; }   
-        public required string Password { get; set; } 
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
