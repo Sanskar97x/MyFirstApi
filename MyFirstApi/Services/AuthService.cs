00d330d94461e92b5f73a5e1044f5d6858618d38
Sanskar97x <tamangsanskar3@gmail.com>
2026-06-17 07:01:08 +0545

    PasswordVerificationResult Success Status Reviewed With JWT Token

--- MyFirstApi/Services/AuthService.cs
+++ MyFirstApi/Services/AuthService.cs
@@ -31,26 +31,6 @@ namespace MyFirstApi.Services
                     tokenDto.Message = "Please Fill All The Details";
                     return new Tuple<int, TokenDto>(1, tokenDto);
                 }
-<<<<<<< HEAD
-=======
-
-                var existingUser = await _context.AccountUsers.FirstOrDefaultAsync(x => x.Email == dto.Email);
-
- 
-                //if (existingUser.Password != dto.Password)
-                //{
-                //    return new Tuple<int, TokenDto>(1, "Password Is Incorrect");
-                //}
-
-                tokenDto.Message = "This User Not Exist, Please Login";
-                return new Tuple<int, TokenDto>(0, tokenDto);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
->>>>>>> 964e578b621e8deeceaa5e7bfb370fff84c00435
 
                 var existingUser = await _context.AccountUsers.FirstOrDefaultAsync(x => x.Email == dto.Email);
 
@@ -69,20 +49,20 @@ namespace MyFirstApi.Services
                 var PasswordHasher = new PasswordHasher<string>();
                 var verifyPassword = PasswordHasher.VerifyHashedPassword(dto.Email, existingUser.Password, dto.Password);
 
-                //if (verifyPassword == PasswordVerificationResult.Success)
-                //{
-                //    UserDto user = new();
-                //    user.Email = dto.Email;
-                //    user.Id = existingUser.Id;
-                //    user.Name = existingUser.Name;
-                //    var token = GetJwtToken(user);
+                if (verifyPassword == PasswordVerificationResult.Success)
+                {
+                    UserDto user = new();
+                    user.Email = dto.Email;
+                    user.Id = existingUser.Id;
+                    user.Name = existingUser.Name;
+                    var token = GetJwtToken(user);
 
-                //    tokenDto.Token = token;
-                //    tokenDto.Message = "Login Successfull";
+                    tokenDto.Token = token;
+                    tokenDto.Message = "Login Successfull";
 
-                //    return new Tuple<int, TokenDto>(2, tokenDto);
+                    return new Tuple<int, TokenDto>(2, tokenDto);
 
-                //}
+                }
                 return new Tuple<int, TokenDto>(2, tokenDto);
             }
             catch
