b1ae27da7785f5be3eaada8eb8f1bc7d3342634d
Sanskar97x <tamangsanskar3@gmail.com>
2026-06-18 15:54:14 +0545

    SuccessRehashNeeded Condition

--- MyFirstApi/Services/AuthService.cs
+++ MyFirstApi/Services/AuthService.cs
@@ -63,6 +63,26 @@ namespace MyFirstApi.Services
                     return new Tuple<int, TokenDto>(2, tokenDto);
 
                 }
+<<<<<<< HEAD
+=======
+                else if (verifyPassword == PasswordVerificationResult.SuccessRehashNeeded)
+                {
+                    UserDto user = new();
+                    user.Email = existingUser.Email;
+                    user.Id = existingUser.Id;
+                    user.Name = existingUser.Name;
+                    var token = GetJwtToken(user);
+
+                    existingUser.Password = PasswordHashing(dto);
+                    _context.AccountUsers.Update(existingUser);
+                    _context.SaveChanges();
+
+                    tokenDto.Token = token;
+                    tokenDto.Message = "Login Successful, New Hash Generated";
+
+                    return new Tuple<int, TokenDto>(2, tokenDto);
+                }
+>>>>>>> 220f8bc (Condition For SuccessRehashNeeded)
 
                 tokenDto.Message = "This User Not Exist, Please Login";
                 return new Tuple<int, TokenDto>(0, tokenDto);
